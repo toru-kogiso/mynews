2ac80cd0828d051ceb2197039f6976d4e2dcfd21
toru <el.9.ogi@gmail.com>
2020-11-23 11:01:31 +0000

    task16/ニュース更新削除

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,19 @@ Route::group(['prefix' => 'admin', 'middleware' =>'auth'], function(){
     Route::post('profile/create', 'Admin\ProfileController@create');
     Route::post('profile/edit', 'Admin\ProfileController@updata');
     Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit'); 
+    Route::post('profile/edit', 'Admin\ProfileController@update'); 
+
 });
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+});
