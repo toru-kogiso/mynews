16639d06272615f65837861bc0530770d172a070
toru <el.9.ogi@gmail.com>
2020-12-02 11:10:09 +0000

    画像テスト

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -14,7 +14,7 @@
                              <div class="caption mx-auto">
                                   <div class="image">
                                    @if ($headline->image_path)
-                                      <img src="{{ asset('storage/image/' . $headline->image_path) }}">
+                                      <img src="{{ $headline->image_path }}">
                                    @endif
                                 </div>
                                 <div class="title p-2">
@@ -48,7 +48,7 @@
                            </div>
                            <div class="image col-md-6 text-right mt-4">
                                @if ($post->image_path)
-                                   <img src="{{ asset('storage/image/' . $post->image_path) }}">
+                                   <img src="{{ $post->image_path }}">
                                 @endif       
                            </div>
                        </div>
